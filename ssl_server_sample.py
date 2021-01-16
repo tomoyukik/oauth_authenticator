c71ae1a08d4f3df29d630b5b3798eb3b291060d8
tomoyukik <kobori.tomoyuki@gmail.com>
2021-01-16 19:13:11 +0900

    update: separate post request

--- ssl_server_sample.py
+++ ssl_server_sample.py
@@ -26,7 +26,7 @@ MAUTIC_API_BASE_URL = 'https://0.0.0.0'
 CLIENT_ID = '1_5j8ecbsu9cowo4wk8kwwcc8k8wc08c8o4sgo4s084cg880ggo0'
 CLIENT_SECRET = '172h8p6mevy8w8cggc44gw4w4ookk4ockg440osggkw808c00g'
 
-class GetHandler(BaseHTTPRequestHandler):
+class AuthenticationHandler(BaseHTTPRequestHandler):
     def __init__(self, request, address, server, client_id, client_secret):
         print('init')
         self._client_id = client_id
@@ -41,23 +41,12 @@ class GetHandler(BaseHTTPRequestHandler):
         self.end_headers()
 
         if 'code' in self.path:
-            params = self.path.split('&')
-            state = params[0].replace('/?state=', '')
-            code = params[1].replace('code=', '')
-            url = f'{MAUTIC_API_BASE_URL}/oauth/v2/token'
-            headers = {'Content-Type': 'application/json'}
-            params = {
-                'client_id': self._client_id,
-                'client_secret': self._client_secret,
-                'grant_type': 'authorization_code',
-                'redirect_uri': 'https://0.0.0.0:8888',
-                'code': code
-            }
-            response = requests.post(url, data=params, verify=False)
             self.server.access_token = None
 
+            response = self._send_post_request()
             print('response_status code:', response.status_code)
             print('response content:', response.content)
+
 
             if response.status_code == 200:
                 print('response json:', response.json)
@@ -69,6 +58,83 @@ class GetHandler(BaseHTTPRequestHandler):
         self.wfile.write(bytes('gomengo', 'utf-8'))
         return
 
+    def _send_post_request(self):
+        params = self.path.split('&')
+        state = params[0].replace('/?state=', '')
+        code = params[1].replace('code=', '')
+
+        access_url = f'{MAUTIC_API_BASE_URL}/oauth/v2/token'
+        headers = {'Content-Type': 'application/json'}
+        params = {
+            'client_id': self._client_id,
+            'client_secret': self._client_secret,
+            'grant_type': 'authorization_code',
+            'redirect_uri': 'https://0.0.0.0:8888',
+            'code': code
+        }
+        response = requests.post(access_url, data=params, verify=False)
+        return response
+
+
+class AccessTokenHandler:
+
+    def __init__(self, client_id, client_secret):
+        self._client_id = client_id
+        self._client_secret = client_secret
+
+    def get_access_token(self):
+        token = None
+        host = '0.0.0.0'
+        port = 8888
+        grant_type = 'authorization_code'
+        app_url = 'https://0.0.0.0:8888'
+        response_type = 'code'
+        authorize_path = '/oauth/v2/authorize'
+
+        handler = lambda request, address, server: AuthenticationHandler(
+            request, address, server, CLIENT_ID, CLIENT_SECRET
+        )
+
+        context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+        context.load_cert_chain('./ssl_test.crt', keyfile='./ssl_test.key')
+        context.options |= ssl.OP_NO_TLSv1 | ssl.OP_NO_TLSv1_1
+
+        params = {
+            'client_id': CLIENT_ID,
+            'grant_type': grant_type,
+            'redirect_uri': urllib.parse.quote(app_url, safe=''),
+            'response_type': response_type,
+            'state': self._randomname(40)
+        }
+        access_url = self._join_url(MAUTIC_API_BASE_URL, authorize_path, params)
+        open_new(access_url)
+
+        with HTTPServer((host, port), handler) as server:
+            server.socket = context.wrap_socket(server.socket)
+            print('Server Starts - %s:%s' % (host, port))
+
+            try:
+                while token is None:
+                    server.access_token = None
+                    server.handle_request()
+                    token = server.access_token
+            except KeyboardInterrupt:
+                pass
+
+        print('アクセストークン取れたよー', token)
+        print('Server Stops - %s:%s' % (host, port))
+        return token
+
+    def _randomname(self, n):
+        randlst = [random.choice(string.ascii_letters + string.digits) for i in range(n)]
+        return ''.join(randlst)
+
+    def _join_url(self, base_url, sub_directory, params):
+        params_joined = '&'.join(['='.join(p) for p in params.items()])
+        joined_url = ''.join([base_url, sub_directory])
+        return '?'.join([joined_url, params_joined])
+
+
 def randomname(n):
     randlst = [random.choice(string.ascii_letters + string.digits) for i in range(n)]
     return ''.join(randlst)
@@ -79,23 +145,37 @@ def join_url(base_url, sub_directory, params):
     return '?'.join([joined_url, params_joined])
 
 
-def run(host, port, ctx, handler):
-    token = None
+def get_access_token():
+    host = '0.0.0.0'
+    port = 8888
+    grant_type = 'authorization_code'
+    app_url = 'https://0.0.0.0:8888'
+    response_type = 'code'
+    authorize_path = '/oauth/v2/authorize'
+
+    handler = lambda request, address, server: AuthenticationHandler(
+        request, address, server, CLIENT_ID, CLIENT_SECRET
+    )
+
+    context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+    context.load_cert_chain('./ssl_test.crt', keyfile='./ssl_test.key')
+    context.options |= ssl.OP_NO_TLSv1 | ssl.OP_NO_TLSv1_1
 
     params = {
         'client_id': CLIENT_ID,
-        'grant_type': 'authorization_code',
-        'redirect_uri': urllib.parse.quote('https://0.0.0.0:8888', safe=''),
-        'response_type': 'code',
+        'grant_type': grant_type,
+        'redirect_uri': urllib.parse.quote(app_url, safe=''),
+        'response_type': response_type,
         'state': randomname(40)
     }
-    access_url = join_url(MAUTIC_API_BASE_URL, '/oauth/v2/authorize', params)
+    access_url = join_url(MAUTIC_API_BASE_URL, authorize_path, params)
     open_new(access_url)
 
+    # クライアントサーバ起動
+    token = None
     with HTTPServer((host, port), handler) as server:
-        server.socket = ctx.wrap_socket(server.socket)
+        server.socket = context.wrap_socket(server.socket)
         print('Server Starts - %s:%s' % (host, port))
-
         try:
             while token is None:
                 server.access_token = None
@@ -107,14 +187,13 @@ def run(host, port, ctx, handler):
     print('アクセストークン取れたよー', token)
     print('Server Stops - %s:%s' % (host, port))
 
+# mauticにアクセス(GET リクエスト)
+# サーバ起動
+# レスポンスを取得
+# POSTリクエスト
+# レスポンス取得
+
 if __name__ == '__main__':
-    host = '0.0.0.0'
-    port = 8888
-
-    ssl._create_default_https_context = ssl._create_unverified_context
-    ctx = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
-    ctx.load_cert_chain('./ssl_test.crt', keyfile='./ssl_test.key')
-    ctx.options |= ssl.OP_NO_TLSv1 | ssl.OP_NO_TLSv1_1
-    handler = lambda req, add, ser: GetHandler(req, add, ser, CLIENT_ID, CLIENT_SECRET)
-
-    run(host, port, ctx, handler)
+    # get_access_token()
+    handler = AccessTokenHandler(CLIENT_ID, CLIENT_SECRET)
+    print('だから取れたぞー', handler.get_access_token())
